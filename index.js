9a5406ac9b34452d723c9b1d11eb8c88253e9373
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-04 16:18:57 -0400

    working version

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+
+let dodger = document.getElementById(`dodger`);
+
+function moveLeft() {
+  let left = parseInt(dodger.style.left.replace(`px`, ``));
+
+  if (left > 0) {
+    dodger.style.left = `${left - 4}px`;
+  }
+};
+
+function moveRight() {
+  let left = parseInt(dodger.style.left.replace(`px`, ``));
+
+  if (left < 360) {
+    dodger.style.left = `${left + 4}px`;
+  }
+}
+
+
+dodger.style.backgroundColor = `#FF69B4`;
+
+document.addEventListener(`keydown`, function(pressed) {
+  if (pressed.key === `ArrowLeft`) {
+    moveLeft();
+  } else if (pressed.key === `ArrowRight`) {
+    moveRight();
+  }
+});
